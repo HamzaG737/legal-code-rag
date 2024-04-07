30292b124ee645a944991fa8af959d54f424c499
HamzaG737 <hamza96gharbi@gmail.com>
2024-04-07 12:24:01 +0200

    Improve translation process

--- query/query_engine.py
+++ query/query_engine.py
@@ -15,7 +15,7 @@ def update_prompts_for_query_engine(query_engine: BaseQueryEngine) -> BaseQueryE
         "---------------------\n"
         "Given the context information and not prior knowledge, answer the query.\n"
         "You must always mention the code name and article number in your answer. With something like 'selon le code x et l'article y, ...'\n"
-        "Your response must be in the original language of the query. For instance if the query is in english, you must respond in english.\n"
+        "You must also detect the language of the query and translate your response to the original query language. \n"
         "Query: {query_str}\n"
         "Answer: "
     )
